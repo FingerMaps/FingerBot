8aaddb41dae38c4ca31cbf62af4671a76e706e3b
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-09-21 22:23:13 +0100

    fix updaterr

--- commands/updaterr.js
+++ commands/updaterr.js
@@ -5,25 +5,24 @@ module.exports = {
     cooldown: 5,
     execute (message, args) {
         if (message.member.roles.cache.some(role=>role.id==roleIDs.team)) {
-			message.guild.channels.cache.get(channelIDs.information).messages.fetch("757710945853112502")({embed: {
-				color: color,
-				description: "React with one of the following emojis to get certain roles. You will be pinged with every announcement or sneakpeek.",
-				fields: [
-					{
-						name: "📢",
-						value: "Announcements",
-						inline: true
-					},
-					{
-						name: "📷",
-						value: "Sneakpeeks",
-						inline: true
-					}
-				]
-			}}).then(msg=>{
-				// msg.react("📢")
-				// msg.react("📷")
-				message.react("✅")
+			message.guild.channels.cache.get(channelIDs.information).messages.fetch("757710945853112502").then(msg=>{
+				msg.edit({embed: {
+					color: color,
+					title: "Notification Roles",
+					description: "React with one of the following emojis to get certain roles. You will be pinged with every announcement or sneakpeek.",
+					fields: [
+						{
+							name: "📢",
+							value: "Announcements",
+							inline: true
+						},
+						{
+							name: "📷",
+							value: "Sneakpeeks",
+							inline: true
+						}
+					]
+				}}).then(()=>message.react("✅"))
 			})
 		} else message.react("🚫")
     }
