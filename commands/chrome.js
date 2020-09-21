8f3496c4de576fe645b207cae02e2ea1f0eb7d53
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-09-21 22:56:39 +0100

    Revert "make chrome tutorial embedded"
    This reverts commit 7a9ce9a92de0aafc3f41521b44218de22b1b0a7b.

--- commands/chrome.js
+++ commands/chrome.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "chrome",
     cooldown: 5,
     execute (message, args) {
-        message.channel.send("Need help installing a Chrome Theme? Here's a tutorial https://youtu.be/8elax6XIzpg")
+        message.channel.send("Need help installing a Chrome Theme? Here's a tutorial <https://youtu.be/8elax6XIzpg>")
         message.delete()
     }
 }
