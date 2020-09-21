ac748b6b7d53b255073e45b00e2e6226cb5d1eab
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-09-21 20:34:36 +0100

    fix bedrock command

--- commands/bedrock.js
+++ commands/bedrock.js
@@ -1,5 +1,7 @@
-export const name = "bedrock"
-export const cooldown = 5
-export function execute(message, args) {
-    message.channel.send("We do not support Bedrock Editions of Minecraft due to its differences to Java Edition, most notably the command differences.")
+module.exports = {
+    name: "bedrock",
+    cooldown: 5,
+    execute (message, args) {
+        message.channel.send("We do not support Bedrock Editions of Minecraft due to its differences to Java Edition, most notably the command differences.")
+    }
 }
