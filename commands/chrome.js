59c660eccf768fafc2edb91d501e2e94a1ba1b4b
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-09-21 22:43:15 +0100

    add -chrome

---
+++ commands/chrome.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "chrome",
+    cooldown: 5,
+    execute (message, args) {
+        message.channel.send("Need help installing a Chrome Theme? Here's a tutorial <https://youtu.be/8elax6XIzpg>")
+        message.delete()
+    }
+}
