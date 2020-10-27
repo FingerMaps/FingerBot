261fa5c9548741232ed4b02a7b948ba408e2ab08
Beatso <39217836+Beatso@users.noreply.github.com>
2020-10-27 18:23:57 +0000

    Update feedback.js

--- commands/feedback.js
+++ commands/feedback.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "feedback",
     cooldown: 5,
     execute (message, args) {
-        message.channel.send("Have an idea? Go submit your feedback here: <https://www.fingermaps.net/feedback>")
+        message.channel.send("Have an idea? Go submit your feedback here: <https://www.fingermaps.net/contact>")
         message.delete()
     }
 }
