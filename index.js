94911b41cf2769410c74401ea3d637149c24cc31
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-11-02 22:45:33 +0000

    change status

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const cooldowns = new Discord.Collection()
 
 client.once("ready", () => {
 	console.log("bot running")
-	client.user.setActivity("Floreign Complex")
+	client.user.setActivity("Solar Swarm")
 })
 
 client.on("message", message => {
